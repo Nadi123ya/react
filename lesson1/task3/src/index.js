78b55be21b4d310a7ac2f20c570e7a9179f42c41
Nadi123ya <nadezhda.bdarenko.96@gmail.com>
2022-09-06 17:51:00 +0400

    commited

--- lesson1/task3/src/index.js
+++ lesson1/task3/src/index.js
@@ -9,7 +9,7 @@ const rootElement = document.querySelector('#root');
 const element = (
   <div className="greeting">
     <div className="greeting__title">Hello, world!</div>
-    <div className="greeting__text">I`m learning Reac</div>
+    <div className="greeting__text">I'm learning Reac</div>
   </div>
 
 );
