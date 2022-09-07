ceb261fac9c01040aa1e7773ac3cb7c2c5a3a92f
Nadi123ya <nadezhda.bdarenko.96@gmail.com>
2022-09-07 12:56:32 +0400

    commited

---
+++ lesson3/task3/src/Avatar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const UserAvatar = (props) => {
+  return (
+    <img className="avatar" src={props.avatarUrl} alt={props.name} />
+  );
+};
+
+export default UserAvatar;
