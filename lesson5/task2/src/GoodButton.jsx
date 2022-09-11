61899c7a2269356ff3cf969243d19e976ce48ae5
Nadi123ya <nadezhda.bdarenko.96@gmail.com>
2022-09-11 12:18:32 +0400

    commited

--- lesson5/task2/src/GoodButton.jsx
+++ lesson5/task2/src/GoodButton.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 class GoodButton extends React.Component {
-    handleClick(e){
-        console.log(e.target.textContent);
-        console.log(es);
-        alert("Good job!")
-    }
+  handleClick(e) {
+    alert(`${e.target.textContent}`);
+  }
 
   render() {
     return (
-      <button className="fancy-button" onClick={this.handleClick} >
+      <button className="fancy-button" onClick={this.handleClick}>
         Click me!
       </button>
     );
