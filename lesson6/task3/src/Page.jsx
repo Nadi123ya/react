7ac9769e43932be82b9bd4c9367417cebb6742dd
Nadi123ya <nadezhda.bdarenko.96@gmail.com>
2022-09-12 13:20:57 +0400

    commited

--- lesson6/task3/src/Page.jsx
+++ lesson6/task3/src/Page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Message from "./Message";
 
 const text1 = "Hello, world!";
-const text2 = "Another excisting text.";
+const text2 = "Another exciting text.";
 
 class Page extends React.Component {
   state = {
