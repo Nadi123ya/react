2aea1e5352b30562ab3291f06d0ef1b4c24ec175
Nadi123ya <nadezhda.bdarenko.96@gmail.com>
2022-09-20 22:29:12 +0400

    commited

---
+++ lesson8/task4/src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import User from "./User.jsx"
+
+const rootElem = document.querySelector("#root");
+
+
+ReactDOM.render(<User userId="facebook" />, rootElem);
