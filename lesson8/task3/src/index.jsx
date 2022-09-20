18d92be74ed23c7e6b5cb05eb6ed111623cd77b6
Nadi123ya <nadezhda.bdarenko.96@gmail.com>
2022-09-20 21:56:22 +0400

    commited

---
+++ lesson8/task3/src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import Dimensions from "./Dimensions.jsx"
+
+const rootElem = document.querySelector("#root");
+
+
+ReactDOM.render(<Dimensions />, rootElem);
