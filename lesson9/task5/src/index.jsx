b96cf745d719864bd3fbc7a8a1881f74f27cc92e
Nadi123ya <nadezhda.bdarenko.96@gmail.com>
2022-09-22 11:33:04 +0400

    commited

---
+++ lesson9/task5/src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Game from "./Game";
+import "./index.scss";
+
+
+const rootElem = document.querySelector("#root");
+
+ReactDOM.render(<Game />, rootElem)
