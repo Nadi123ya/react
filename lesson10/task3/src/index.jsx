2e604e0f45fd8af0c89fdef134d7d91a74131e9b
Nadi123ya <nadezhda.bdarenko.96@gmail.com>
2022-09-29 22:39:19 +0400

    commited

---
+++ lesson10/task3/src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Calculator from "./Calculator";
+
+const rootElem = document.querySelector("#root");
+
+ReactDOM.render(<Calculator userId="github" />, rootElem);
